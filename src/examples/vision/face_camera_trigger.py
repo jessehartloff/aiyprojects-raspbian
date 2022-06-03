892f0b074305eccd262abd346a5bb266404a8fb4
Jesse Hartloff <hartloff@buffalo.edu>
2022-06-03 14:58:41 -0400

    Add filename generator for images -Modified from RickRoller code

--- src/examples/vision/face_camera_trigger.py
+++ src/examples/vision/face_camera_trigger.py
@@ -14,28 +14,40 @@
 # limitations under the License.
 """Trigger PiCamera when face is detected."""
 
+import vlc
+import time
+from datetime import datetime
+
 from aiy.vision.inference import CameraInference
 from aiy.vision.models import face_detection
 
 from picamera import PiCamera
 
+def generate_filename():
+    timestamp = datetime.now()
+    return "image_" + timestamp.strftime("%Y-%m-%d_%H:%M:%S.%f") + ".jpg"
+
+def test_filenames():
+    for i in range(50):
+        print(generate_filename())
+        time.sleep(0.3)
 
 def main():
     with PiCamera() as camera:
         # Configure camera
         camera.resolution = (1640, 922)  # Full Frame, 16:9 (Camera v2)
-        camera.start_preview()
 
         # Do inference on VisionBonnet
         with CameraInference(face_detection.model()) as inference:
             for result in inference.run():
                 if len(face_detection.get_faces(result)) >= 1:
-                    camera.capture('faces.jpg')
-                    break
-
-        # Stop preview
-        camera.stop_preview()
+                    camera.capture(generate_filename())
+                    player = vlc.MediaPlayer("/home/pi/Music/Rick2.mp3")
+                    player.play()
+                    time.sleep(65)
 
 
 if __name__ == '__main__':
     main()
+    # test_filenames()
+
